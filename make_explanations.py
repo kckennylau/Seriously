56a8d2db3486b4d63954bd347b7db881227cfe97
Ryan Rowe <Mego@users.noreply.github.com>
2016-01-08 21:13:38 -0600

    preliminary unicode support

--- make_explanations.py
+++ make_explanations.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 
+import re, codecs
+
 def make_explanations():
     lines = []
-    with open('commands.txt','Urb') as f:
+    with codecs.open('commands.txt','Urb',encoding='utf-8') as f:
         lines = f.read().split('\n')[:256]
     ex = "var explanations = {\n%s\n};"
     exps = []
+    rg = re.compile(r'^[0-9A-F]{2} \((.+)\): (.*)$')
     for line in lines:
-        toks = line.split(':')
-        val,desc = toks[0],':'.join(toks[1:])
-        val = val[:val.index(' ')]
-        desc = repr(desc.strip())
-        exps.append('"%s":%s'%(val,desc))
-    with open('static/explanations.js','wb') as f:
-        f.write(ex%(',\n'.join(exps)))
+        res = rg.search(line)
+        if res:
+            val, desc = res.groups()
+            exps.append('%s:%s'%(`val`[1:],`desc`[1:]))
+    with codecs.open('static/explanations.js','wb',encoding='utf-8') as f:
+        f.write(ex%(u',\n'.join(exps)))
         
 if __name__ == '__main__':
     make_explanations()
